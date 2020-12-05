dd723d795c6941b729ec1d07a6bc485729312d46
Matthew Jorgensen <matthew@jrgnsn.net>
2020-12-05 11:30:02 -0600

    Working on day2...

---
+++ Day Two/two.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "regexp"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    file, err := os.Open("input")
+    if err != nil {
+        fmt.Println("File reading error:", err)
+        return
+    }
+    defer file.Close()
+
+    type password struct {
+        Min, Max int
+        Password string
+        Policy string
+    }
+
+    var passwords = make([]password, 0)
+    re := regexp.MustCompile(`(\d*)-(\d*)\s(\w):\s(\w*)`)
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        result := re.FindStringSubmatch(scanner.Text())
+        min, err := strconv.Atoi(result[1])
+        if err != nil {
+            fmt.Println("Integer conversion error:", err)
+        }
+        max, err := strconv.Atoi(result[2])
+        if err != nil {
+            fmt.Println("Integer conversion error:", err)
+        }
+        passwords = append(passwords, password{
+            Min: min,
+            Max: max,
+            Policy: result[3],
+            Password: result[4],
+        })
+
+    }
+    good := 0
+    for _, a := range passwords {
+        count := strings.Count(a.Password, a.Policy)
+        if count >= a.Min && count <= a.Max {
+            good++
+        }
+    }
+    fmt.Println("Total good passwords:", good)
+    good1 := 0
+    for _, a := range passwords {
+        //index := strings.IndexAny(a.Policy, a.Password)+1
+        for i, c := range a.Password {
+            i++
+            if string(c) == a.Policy {
+                if (i == a.Min) && (i == a.Max) {
+                    continue
+                } else {
+                    good1++
+                }
+            }
+        }
+    }
+    fmt.Println("Total actually good1 passwords:", good)
+}
